b68870e666dc180d468a27b311812c27d0cc5239
JHSeong <50869470+hoper7748@users.noreply.github.com>
2024-10-15 23:03:01 +0900

    Update 2024-10-15
    1. Upgrade UI 로직 임시 제작

---
+++ Project A/Assets/Scripts/MVP/Presenter/Upgrade Presenter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MVP.Upgrade
+{
+    public class UpgradePresenter : MonoBehaviour
+    {
+        UpgradeModel upgradeModel;
+        UpgradeView upgradeView;
+
+
+        // Getter
+        // 업그레이드가 될 때마다 Manager과 View / Model에 업데이트 해주기.
+        public void UpdateConponent()
+        {
+
+        }
+
+
+
+    }
+
+}
